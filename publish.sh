15d93df36c66418fcb693e83e269333b51199cfc
splincode <omaxphp@yandex.ru>
2020-06-02 15:17:16 +0300

    chore: fixup

--- publish.sh
+++ publish.sh
@@ -2,3 +2,13 @@
 
 echo "Commit: $TRAVIS_COMMIT";
 echo "Commit message: $TRAVIS_COMMIT_MESSAGE";
+
+if [ -n "$NPM_TOKEN" ]; then
+  npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
+fi
+
+# shellcheck disable=SC2039
+if [[ $TRAVIS_COMMIT_MESSAGE != *"publish"* ]]; then
+  echo "Publish...."
+  yarn lerna publish --yes
+fi;
