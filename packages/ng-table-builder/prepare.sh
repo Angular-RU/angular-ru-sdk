34849ba7dd4897f54163a0b9807ab90e29e9c691
splincode <omaxphp@yandex.ru>
2020-06-23 22:51:22 +0300

    chore: remove redundant copy

--- packages/ng-table-builder/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p node_modules
-cp -v -r ../../node_modules/@angular node_modules
-cp -v -r ../../node_modules/hammerjs node_modules
-cp -v -r ../../node_modules/ngx-toastr node_modules
