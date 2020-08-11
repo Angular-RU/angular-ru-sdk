0dc9e10047d38f78db3c7deeeefe946d21161646
splincode <omaxphp@yandex.ru>
2020-08-11 18:29:14 +0300

    !fixup

--- packages/common/http/src/is-localhost.ts
+++ packages/common/http/src/is-localhost.ts
@@ -1,4 +1,3 @@
 export function isLocalhost(url: string = location.href): boolean {
-    console.log('href', location.href)
     return url.includes('://localhost') || url.includes('://127.0.0.1') || url.includes('://0.0.0.0');
 }
