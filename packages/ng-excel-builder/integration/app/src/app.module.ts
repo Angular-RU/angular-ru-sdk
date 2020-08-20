0a32bb9b2db7453d20030b048a5586dc73abdead
splincode <omaxphp@yandex.ru>
2020-08-20 15:51:49 +0300

    !fixup

--- packages/ng-excel-builder/integration/app/src/app.module.ts
+++ packages/ng-excel-builder/integration/app/src/app.module.ts
@@ -2,9 +2,9 @@ import { ExcelBuilderModule } from '@angular-ru/ng-excel-builder';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
-import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
     bootstrap: [AppComponent],
