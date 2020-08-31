3295502d567c027f745414511c25a5a848b1071d
splincode <omaxphp@yandex.ru>
2020-08-31 14:12:17 +0300

    !fixup

--- packages/flex-layout/integration/app/src/samples/guide/guide.component.ts
+++ packages/flex-layout/integration/app/src/samples/guide/guide.component.ts
@@ -1,8 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Any } from '@angular-ru/common/typings';
+import { AfterViewInit, ChangeDetectionStrategy, Component } from '@angular/core';
+
+declare const hljs: Any;
 
 @Component({
     selector: 'guide',
     templateUrl: './guide.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GuideComponent {}
+export class GuideComponent implements AfterViewInit {
+    public ngAfterViewInit(): void {
+        document.querySelectorAll('pre code').forEach((block: Any): void => {
+            hljs.highlightBlock(block);
+        });
+    }
+}
