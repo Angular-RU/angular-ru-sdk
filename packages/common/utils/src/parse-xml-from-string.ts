238a9017e8579a19bbdfe8a30cbd73edb4b862ee
splincode <omaxphp@yandex.ru>
2020-09-03 17:17:50 +0300

    fix(@angular-ru/common): support TypeScript 4

--- packages/common/utils/src/parse-xml-from-string.ts
+++ packages/common/utils/src/parse-xml-from-string.ts
@@ -1,4 +1,6 @@
-export function parseXmlFromString(xml: string, type: SupportedType = 'application/xml'): Document | never {
+type XmlSupportedType = 'application/xhtml+xml' | 'application/xml' | 'image/svg+xml' | 'text/html' | 'text/xml';
+
+export function parseXmlFromString(xml: string, type: XmlSupportedType = 'application/xml'): Document | never {
     const parser: DOMParser = new DOMParser();
     const dom: Document = parser.parseFromString(xml, type);
 
