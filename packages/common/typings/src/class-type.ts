e45adb88225a12476663d56090710b10b614fc8b
m.ivanov4 <omaxphp@yandex.ru>
2020-09-07 16:01:49 +0300

    !fixup

--- packages/common/typings/src/class-type.ts
+++ packages/common/typings/src/class-type.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Any } from './any';
 
 // eslint-disable-next-line @typescript-eslint/type-annotation-spacing,@typescript-eslint/ban-types
