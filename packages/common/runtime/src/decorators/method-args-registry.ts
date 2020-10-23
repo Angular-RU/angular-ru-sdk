b67334daa4b1cecea7b9a4db4470d46bbf460c4c
m.ivanov4 <omaxphp@yandex.ru>
2020-10-23 09:48:13 +0300

    fix(@angular-ru/common): refactor runtime methodArgsRegistry

--- packages/common/runtime/src/decorators/method-args-registry.ts
+++ packages/common/runtime/src/decorators/method-args-registry.ts
@@ -1,31 +1,24 @@
 import { InvalidArgsNamesException } from '../exceptions/invalid-args-names.exception';
 
 export class MethodArgsRegistry {
-    private decoratorMap: Map<number | string, string> = new Map();
-    private argumentMap: Map<number | string, string> = new Map();
+    private argumentsIndexMap: Map<number | string, string> = new Map();
 
-    public getVariableDecoratorTypeByIndex(index: number): string | null {
-        return this.decoratorMap.get(index) ?? null;
+    public get size(): number {
+        return this.argumentsIndexMap.size;
     }
 
-    public getArgumentNameByIndex(index: number): string | null {
-        return this.argumentMap.get(index) ?? null;
+    public getNameByIndex(index: number): string | null {
+        return this.argumentsIndexMap.get(index) ?? null;
     }
 
-    public createVariableDecoratorType(name: string, method: string, paramIndex: number): void {
+    public putIndexByName(name: string, method: string, paramIndex: number): void {
         this.checkDuplicateName(name, method);
-        this.decoratorMap.set(paramIndex, name);
-        this.decoratorMap.set(name, name);
-    }
-
-    public createArgumentName(name: string, method: string, paramIndex: number): void | never {
-        this.checkDuplicateName(name, method);
-        this.argumentMap.set(paramIndex, name);
-        this.argumentMap.set(name, name);
+        this.argumentsIndexMap.set(paramIndex, name);
+        this.argumentsIndexMap.set(name, name);
     }
 
     private checkDuplicateName(name: string, method: string): void | never {
-        if (this.argumentMap.has(name) || this.decoratorMap.has(name)) {
+        if (this.argumentsIndexMap.has(name)) {
             throw new InvalidArgsNamesException(name, method);
         }
     }
