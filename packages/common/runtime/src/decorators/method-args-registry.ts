a2e2a10664924383d4e45e0caec9b976f9dc4734
m.ivanov4 <omaxphp@yandex.ru>
2020-10-23 14:23:32 +0300

    fix(@angular-ru/common): expose MethodArgsRegistryMeta

--- packages/common/runtime/src/decorators/method-args-registry.ts
+++ packages/common/runtime/src/decorators/method-args-registry.ts
@@ -1,20 +1,30 @@
 import { InvalidArgsNamesException } from '../exceptions/invalid-args-names.exception';
 
+export interface MethodArgsRegistryMeta {
+    key: string;
+    index: number;
+}
+
 export class MethodArgsRegistry {
-    private argumentsIndexMap: Map<number | string, string> = new Map();
+    private argumentsIndexMap: Map<number | string, { key: string; index: number }> = new Map();
 
     public get size(): number {
         return this.argumentsIndexMap.size;
     }
 
     public getNameByIndex(index: number): string | null {
-        return this.argumentsIndexMap.get(index) ?? null;
+        return this.argumentsIndexMap.get(index)?.key ?? null;
+    }
+
+    public getIndexByKey(key: string): number | null {
+        return this.argumentsIndexMap.get(key)?.index ?? null;
     }
 
     public putIndexByName(name: string, method: string, paramIndex: number): void {
+        const info: MethodArgsRegistryMeta = { key: name, index: paramIndex };
         this.checkDuplicateName(name, method);
-        this.argumentsIndexMap.set(paramIndex, name);
-        this.argumentsIndexMap.set(name, name);
+        this.argumentsIndexMap.set(paramIndex, info);
+        this.argumentsIndexMap.set(name, info);
     }
 
     private checkDuplicateName(name: string, method: string): void | never {
