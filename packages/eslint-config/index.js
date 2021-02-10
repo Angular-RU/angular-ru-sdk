2342bdd1da62ba8483d1efef601327b993e21cd6
m.ivanov4 <omaxphp@yandex.ru>
2021-02-10 10:13:55 +0300

    fix(@angular-ru/eslint-config): set off for `no-unused-vars`

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -351,7 +351,7 @@ module.exports = {
                 '@typescript-eslint/no-parameter-properties': 'off',
                 '@typescript-eslint/no-require-imports': 'off',
                 '@typescript-eslint/no-shadow': ['error'],
-                'no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+                'no-unused-vars': 'off',
                 '@typescript-eslint/no-unused-vars': [
                     'error',
                     {
