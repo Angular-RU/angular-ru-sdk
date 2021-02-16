2b07b70aa4ab0fa87700b352c077dc94e8a9f8c7
Алан Щiрбак <33288503+schirbak@users.noreply.github.com>
2021-02-16 19:52:26 +0300

    revert: feat(@angular-ru/common): add checkValueIsFilled function (#416)
    revert for restart ci pipeline
    
    This reverts commit 5737cda2

--- packages/common/utils/src/check-value-is-filled.ts
+++
@@ -1,7 +0,0 @@
-import { Any } from '@angular-ru/common/typings';
-
-import { checkValueIsEmpty } from './check-value-is-empty';
-
-export function checkValueIsFilled(value: Any): boolean {
-    return !checkValueIsEmpty(value);
-}
