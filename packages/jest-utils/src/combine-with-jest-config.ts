dec1801065685da219bd211dae1e4f5e7d4091e2
m.ivanov4 <omaxphp@yandex.ru>
2021-02-20 16:06:35 +0300

    feat(@angular-ru/jest-utils): use options for combine-with-jest-config.ts

--- packages/jest-utils/src/combine-with-jest-config.ts
+++ packages/jest-utils/src/combine-with-jest-config.ts
@@ -1,16 +1,30 @@
 import type { Config } from '@jest/types';
 
-export function combineWithJestConfig(modulePath: string, options: Config.InitialOptions = {}): Config.InitialOptions {
+export interface CombineWithJestConfig {
+    basePath?: string;
+    jestPresetConfigPath: string;
+    options: Config.InitialOptions;
+}
+
+// eslint-disable-next-line max-lines-per-function
+export function combineWithJestConfig(options: CombineWithJestConfig): Config.InitialOptions {
     let config: Config.InitialOptions = {};
+    const modulePathAbsolute: string = require('path').resolve(
+        options.basePath ?? process.cwd(),
+        options.jestPresetConfigPath
+    );
 
     try {
-        config = require(modulePath);
-        config = Object.assign(config, options);
+        config = require(modulePathAbsolute);
+        config = Object.assign(config, options.options);
     } catch {
-        config = options;
+        config = options.options;
         console.error(
             '[ERROR]: Your parent module was not found, by default you will get config: ',
-            JSON.stringify(config)
+            JSON.stringify(config),
+            '\n',
+            'Your path to jest config: ',
+            modulePathAbsolute
         );
     }
 
--- packages/jest-utils/src/index.ts
+++ packages/jest-utils/src/index.ts
@@ -1,3 +1,3 @@
-export { combineWithJestConfig } from './combine-with-jest-config';
+export { CombineWithJestConfig, combineWithJestConfig } from './combine-with-jest-config';
 export { createTsJestConfig } from './create-ts-jest-config';
 export { JestConfigOptions } from './jest-config.interface';
