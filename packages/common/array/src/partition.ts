dab8ae076c3cb14edda8824433236edea9c50905
Алан Щiрбак <33288503+schirbak@users.noreply.github.com>
2021-03-03 17:04:44 +0300

    fixup! feat(@angular-ru/common): add partition array method

--- packages/common/array/src/partition.ts
+++ packages/common/array/src/partition.ts
@@ -2,13 +2,18 @@ import { Couple } from '@angular-ru/common/typings';
 
 export function partition<T>(
     forArray: T[],
-    callback: (element: T, index?: number, array?: T[]) => boolean
+    predicate: (element: T, index?: number, array?: T[]) => boolean
 ): Couple<T[]> {
     return forArray.reduce(
         (partitioning: Couple<T[]>, element: T, ...rest: [number, T[]]): Couple<T[]> => {
-            partitioning[callback(element, ...rest) ? 0 : 1].push(element);
+            /**
+             * Displays which array of a couple the value should fall into, depending on the predicate.
+             * @type {number}
+             */
+            const fittingToPredicateCoupleIndex: number = predicate(element, ...rest) ? 0 : 1;
+            partitioning[fittingToPredicateCoupleIndex].push(element);
             return partitioning;
         },
-        [[], []]
+        [[], []] as Couple<T[]>
     );
 }
