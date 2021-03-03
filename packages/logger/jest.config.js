efc00c69ecbbec4c4c069d823d1b2b6f6cca867f
m.ivanov4 <omaxphp@yandex.ru>
2021-03-03 17:23:52 +0300

    build(@angular-ru/logger): rename setupJest.ts to setup-jest.ts

--- packages/logger/jest.config.js
+++ packages/logger/jest.config.js
@@ -2,8 +2,6 @@ const createTsJestConfig = require('../jest-utils/lib/index').createTsJestConfig
 const path = require('path');
 
 module.exports = createTsJestConfig({
-    maxWorkers: 2,
-    maxConcurrency: 2,
     cacheDirectory: '../../.cache',
     displayName: '@angular-ru/logger',
     rootDir: path.resolve('.'),
@@ -11,6 +9,6 @@ module.exports = createTsJestConfig({
     collectCoverageFrom: ['<rootDir>/lib/**/*.ts'],
     testMatch: ['<rootDir>/integration/tests/**/*.spec.ts'],
     tsConfigSpecPath: '<rootDir>/integration/tests/tsconfig.spec.json',
-    setupFilesAfterEnv: ['<rootDir>/integration/tests/setupJest.ts'],
+    setupFilesAfterEnv: ['<rootDir>/integration/tests/setup-jest.ts'],
     tsConfigRootPath: path.resolve('./tsconfig.json')
 });
