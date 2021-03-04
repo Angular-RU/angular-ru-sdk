373e215196fb5471134f7af9e77a493a9ef0aef8
m.ivanov4 <omaxphp@yandex.ru>
2021-03-04 14:53:50 +0300

    build: support new jest config

--- packages/common/jest.config.js
+++ packages/common/jest.config.js
@@ -1,14 +1,15 @@
 const createTsJestConfig = require('../jest-utils/lib/index').createTsJestConfig;
-const path = require('path');
 
 module.exports = createTsJestConfig({
     isolatedModules: true,
-    cacheDirectory: '../../.cache',
-    rootDir: path.resolve('.'),
-    displayName: '@angular-ru/common',
-    modulePathIgnorePatterns: ['<rootDir>/dist/'],
-    testMatch: ['<rootDir>/integration/tests/**/*.spec.ts'],
-    setupFilesAfterEnv: ['<rootDir>/integration/tests/setup-jest.ts'],
-    tsConfigRootPath: path.resolve('./integration/tests/tsconfig.spec.json'),
-    collectCoverageFrom: ['<rootDir>/**/*.ts', '!<rootDir>/integration/**', '!<rootDir>/dist/**']
+    tsConfig: './integration/tests/tsconfig.spec.json',
+    jestConfig: {
+        rootDir: '.',
+        cacheDirectory: '../../.cache',
+        displayName: '@angular-ru/common',
+        modulePathIgnorePatterns: ['<rootDir>/dist/'],
+        testMatch: ['<rootDir>/integration/tests/**/*.spec.ts'],
+        setupFilesAfterEnv: ['<rootDir>/integration/tests/setup-jest.ts'],
+        collectCoverageFrom: ['<rootDir>/**/*.ts', '!<rootDir>/integration/**', '!<rootDir>/dist/**']
+    }
 });
--- packages/logger/jest.config.js
+++ packages/logger/jest.config.js
@@ -1,13 +1,14 @@
 const createTsJestConfig = require('../jest-utils/lib/index').createTsJestConfig;
-const path = require('path');
 
 module.exports = createTsJestConfig({
-    cacheDirectory: '../../.cache',
-    displayName: '@angular-ru/logger',
-    rootDir: path.resolve('.'),
-    modulePathIgnorePatterns: ['<rootDir>/dist/'],
-    collectCoverageFrom: ['<rootDir>/lib/**/*.ts'],
-    testMatch: ['<rootDir>/integration/tests/**/*.spec.ts'],
-    setupFilesAfterEnv: ['<rootDir>/integration/tests/setup-jest.ts'],
-    tsConfigRootPath: path.resolve('./integration/tests/tsconfig.spec.json')
+    tsConfig: './integration/tests/tsconfig.spec.json',
+    jestConfig: {
+        rootDir: '.',
+        cacheDirectory: '../../.cache',
+        displayName: '@angular-ru/logger',
+        modulePathIgnorePatterns: ['<rootDir>/dist/'],
+        collectCoverageFrom: ['<rootDir>/lib/**/*.ts'],
+        testMatch: ['<rootDir>/integration/tests/**/*.spec.ts'],
+        setupFilesAfterEnv: ['<rootDir>/integration/tests/setup-jest.ts']
+    }
 });
