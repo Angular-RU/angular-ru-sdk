744b23958a7f77abcaf1f58e0582e7125b03fc0d
m.ivanov4 <omaxphp@yandex.ru>
2021-03-15 17:48:54 +0300

    build: use symlink only common package

--- packages/common/symlink.sh
+++ packages/common/symlink.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# build symlink distributive builds for @angular-ru/common
+# build symlink distributive builds for @angular-ru/common on parent node_modules
+
+echo "creating symlink..."
 
 mkdir -p ../../node_modules/@angular-ru/
 cd ../../node_modules/@angular-ru/ || echo 'failed cd'
