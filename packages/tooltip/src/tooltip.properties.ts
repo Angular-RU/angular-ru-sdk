61108e4ffa2b65cba10de0f299876c4c0d868d9b
m.ivanov4 <omaxphp@yandex.ru>
2021-03-29 15:47:05 +0300

    feat(@angular-ru/tooltip): add ngx-tooltip-text.interceptor.ts

--- packages/tooltip/src/tooltip.properties.ts
+++ packages/tooltip/src/tooltip.properties.ts
@@ -1,8 +1,17 @@
+import { Provider } from '@angular/core';
+
+import { NgxTooltipTextInterceptor } from './ngx-tooltip-text.interceptor';
 import { TooltipOptions } from './tooltip.interfaces';
+import { TOOLTIP_TEXT_INTERCEPTOR_TOKEN } from './tooltip.tokens';
 
 export const TOOLTIP_DEFAULT_OPTIONS: TooltipOptions = {
     cssStyle: '',
     durationBeforeCreate: 200,
     durationAfterDestroy: 1500,
     timeoutForWaitAfterBlurTooltip: 400
+};
+
+export const NGX_TOOLTIP_FALLBACK_PROVIDER: Provider = {
+    provide: TOOLTIP_TEXT_INTERCEPTOR_TOKEN,
+    useClass: NgxTooltipTextInterceptor
 };
