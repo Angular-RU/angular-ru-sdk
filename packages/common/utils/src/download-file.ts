f7155ffe961dc3691290aff8155fba240b46ee91
nikitasadchikov <51880015+nikitasadchikov@users.noreply.github.com>
2021-04-12 18:51:07 +0300

    feat(@angular-ru/common/utils): add downloadFile utility (#517)
    * feat(@angular-ru/common/utils): add downloadFile utility
    
    * fixup!(@angular-ru/common/utils): review refactor
    
    * fixup!(@angular-ru/common/utils): add tests
    
    Co-authored-by: Max Ivanov <omaxphp@yandex.ru>

---
+++ packages/common/utils/src/download-file.ts
@@ -0,0 +1,20 @@
+import { checkEveryValueIsEmpty } from './check-every-value-is-empty';
+
+export interface FileToDownloadInfo {
+    blob: Blob | File;
+    name?: string | null;
+    extension?: string | null;
+}
+
+export function downloadFile(file: FileToDownloadInfo): void {
+    if (checkEveryValueIsEmpty(file.name, file.extension)) {
+        throw new Error('File name or file extension must be provided');
+    }
+    const anchor: HTMLAnchorElement = document.createElement('a');
+    const url: string = window.URL.createObjectURL(file.blob);
+    const filePath: string = [file.name, file.extension].filter(Boolean).join('.');
+    anchor.href = url;
+    anchor.download = filePath;
+    anchor.click();
+    window.URL.revokeObjectURL(url);
+}
