41fe43c850003e583b4346521b9ff10da1712454
Ishmukhametov Mansur <m.ishmukhametov@tinkoff.ru>
2021-04-21 18:39:54 +0300

    refactor: changes after review

--- packages/common/utils/src/check-some-value-is-false.ts
+++ packages/common/utils/src/check-some-value-is-false.ts
@@ -3,8 +3,5 @@ import { Any } from '@angular-ru/common/typings';
 import { isFalse } from './is-false';
 
 export function checkSomeValueIsFalse(...values: Any[]): boolean {
-    if (values.length === 0) {
-        return false;
-    }
     return values.some(isFalse);
 }
