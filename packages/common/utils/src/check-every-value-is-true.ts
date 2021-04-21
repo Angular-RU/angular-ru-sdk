7a54f0e839e3864695cdf4e8c4f432685b4e7c28
Ishmukhametov Mansur <m.ishmukhametov@tinkoff.ru>
2021-04-21 23:04:25 +0300

    refactor: changes after review

--- packages/common/utils/src/check-every-value-is-true.ts
+++ packages/common/utils/src/check-every-value-is-true.ts
@@ -3,8 +3,5 @@ import { Any } from '@angular-ru/common/typings';
 import { isTrue } from './is-true';
 
 export function checkEveryValueIsTrue(...values: Any[]): boolean {
-    if (values.length === 0) {
-        return false;
-    }
-    return values.every(isTrue);
+    return values.length === 0 ? false : values.every(isTrue);
 }
