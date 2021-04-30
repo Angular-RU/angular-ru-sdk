80dbf9ff38d7ccad0befadd5975adeea43de7f9e
Schirbak <33288503+schirbak@users.noreply.github.com>
2021-04-30 12:41:31 +0300

    fixup! feat(@angular-ru/common): add date interval suggestion module

--- packages/common/date/src/date-suggestion/date-suggestion.composer.ts
+++ packages/common/date/src/date-suggestion/date-suggestion.composer.ts
@@ -2,10 +2,9 @@ import { Inject, Injectable, InjectFlags, Injector } from '@angular/core';
 
 import { DateSuggestionStrategy } from './domain/interfaces/date-suggestion-strategy';
 import { SuggestionStrategyDescriptor } from './domain/interfaces/suggestion-strategy-descriptor';
+import { StrategyKey } from './domain/types/strategy-key';
 import { SuggestionStrategyMap } from './domain/types/suggestion-strategy-map';
 import { SUGGESTION_STRATEGY_MAP } from './tokens/suggestion-strategy-map';
-
-type StrategyKey = string | symbol | number;
 
 @Injectable()
 export class DateSuggestionComposer<StrategyKeys extends StrategyKey = StrategyKey> {
---
+++ packages/common/date/src/date-suggestion/domain/types/strategy-key.ts
@@ -0,0 +1,1 @@
+export type StrategyKey = string | symbol | number;
