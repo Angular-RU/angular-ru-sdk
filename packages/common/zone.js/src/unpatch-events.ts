8759cfcfb821cb6c7f29751a7fc6c5ad35b22f70
splincode <omaxphp@yandex.ru>
2021-05-13 15:48:00 +0300

    feat(@angular-ru/common): add unpatch events third party package

--- packages/common/zone.js/src/unpatch-events.ts
+++ packages/common/zone.js/src/unpatch-events.ts
@@ -1,44 +1,9 @@
-import { Any } from '@angular-ru/common/typings';
+import { unpatchEvents } from '@angular-ru/common/zone.js/unpatch-zone';
 
+/**
+ * @deprecated: use `import "@angular-ru/common/zone.js/unpatch-zone" before import "zone.js"`
+ */
 // eslint-disable-next-line max-lines-per-function
 export function unpatchZoneJsEvents(): void {
-    if (typeof window !== 'undefined') {
-        (window as Any)['__zone_symbol__UNPATCHED_EVENTS'] = [
-            'scroll',
-            'mouseenter',
-            'mouseleave',
-            'mousemove',
-            'mouseover',
-            'mouseout',
-            'mousewheel'
-        ];
-
-        // black list scroll event handler for onProp
-        const targets: Any[] = [
-            window,
-            Document.prototype,
-            HTMLBodyElement.prototype,
-            HTMLElement.prototype,
-            document.body
-        ];
-
-        (window as Any)['__Zone_ignore_on_properties'] = [];
-
-        targets.forEach((target: Any): void => {
-            (window as Any)['__Zone_ignore_on_properties'].push({
-                target,
-                ignoreProperties: [
-                    'scroll',
-                    'mouseenter',
-                    'mouseleave',
-                    'mousemove',
-                    'mouseover',
-                    'mouseout',
-                    'mousewheel',
-                    'dragover',
-                    'drop'
-                ]
-            });
-        });
-    }
+    unpatchEvents();
 }
