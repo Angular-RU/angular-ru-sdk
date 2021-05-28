23530bb2fd2d6622cce6b6919b62a0bc0c420ddd
Schirbak <33288503+schirbak@users.noreply.github.com>
2021-05-28 10:23:46 +0300

    fixup! feat(@angular-ru/common): add Includes pipe

--- packages/common/pipes/src/includes/includes.pipe.ts
+++ packages/common/pipes/src/includes/includes.pipe.ts
@@ -3,10 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'includes' })
 export class IncludesPipe implements PipeTransform {
     public transform<T>(array: T[] | null | undefined, entry: T): boolean {
-        if (array) {
-            return array.includes(entry);
-        } else {
-            return false;
-        }
+        return array?.includes(entry) ?? false;
     }
 }
