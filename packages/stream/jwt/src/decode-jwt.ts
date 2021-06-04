1e6d8abd08bfe5824168d27ccebfea73457a4396
Nikita Sadchikov <fiveoufive@gmail.com>
2021-06-04 11:27:57 +0300

    fixup! refactor(@angular-core/jwt): fix after review

--- packages/stream/jwt/src/decode-jwt.ts
+++ packages/stream/jwt/src/decode-jwt.ts
@@ -1,4 +1,5 @@
 import { secondItem } from '@angular-ru/common/array';
+import { tryParseJson } from '@angular-ru/common/utils';
 
 export function decodeJwt<T>(token: string | null): T | null {
     let result: T | null = null;
@@ -6,8 +7,8 @@ export function decodeJwt<T>(token: string | null): T | null {
     if (typeof token === 'string') {
         const base64Url: string = secondItem(token.split('.')) || '';
         const base64: string = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const decodedStringValue: string | null = decodeURIComponent(escape(window.atob(base64))) || null;
-        result = decodedStringValue ? JSON.parse(decodedStringValue) : null;
+        const decodedStringValue: string = decodeURIComponent(escape(window.atob(base64)));
+        result = tryParseJson(decodedStringValue) ?? null;
     }
 
     return result;
