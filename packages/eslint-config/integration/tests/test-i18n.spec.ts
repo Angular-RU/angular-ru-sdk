a55025f3cdc1af6c35f3645b4536ca74326092fa
Ishmukhametov Mansur <m.ishmukhametov@tinkoff.ru>
2021-06-11 12:41:50 +0300

    fix(eslint-config): changed tests

--- packages/eslint-config/integration/tests/test-i18n.spec.ts
+++ packages/eslint-config/integration/tests/test-i18n.spec.ts
@@ -7,8 +7,6 @@ describe('[TEST]: Angular-RU eslint recommendations for i18n', (): void => {
 
     it('check failed files', (): void => {
         const bad: string = getInfoByReportFile('bad');
-        expect(bad.includes('i18n\\a\\bad.json')).toBeTruthy();
-        expect(bad.includes('i18n\\bad.json')).toBeTruthy();
         expect(bad.includes('2 problems (2 errors, 0 warnings)')).toBeTruthy();
     });
 
