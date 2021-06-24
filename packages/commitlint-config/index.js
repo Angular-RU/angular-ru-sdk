748cd8a279fa2a6606691b44ed5bd50c3547109a
StepaZol <66434560+StepaZol@users.noreply.github.com>
2021-06-24 13:40:45 +0300

    ci: commitlint head-max-length 100 (#698)
    * ci: commitlint head-max-length 100
    
    * ci: commitlint head-max-length 100
    
    * Revert "ci: commitlint head-max-length 100"
    
    This reverts commit b1983740
    
    Co-authored-by: Stepan Zolin <stepanzol@gmail.com>

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional', '@commitlint/config-angular'],
     rules: {
+        'header-max-length': [2, 'always', 100],
         'subject-case': [0]
     }
 };
