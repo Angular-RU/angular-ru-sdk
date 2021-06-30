c6d44dc38b2aefd08416d5a8a13f2439a35314e5
Max Ivanov <omaxphp@yandex.ru>
2021-06-30 18:43:46 +0300

    feat(@angular-ru/common): improved tryParseJson (#708)

--- packages/common/utils/src/try-parse-json.ts
+++ packages/common/utils/src/try-parse-json.ts
@@ -1,8 +1,8 @@
 import { Any, Nullable } from '@angular-ru/common/typings';
 
-export function tryParseJson<T = Any>(text: string): Nullable<T> {
+export function tryParseJson<T>(text: Nullable<string>): Nullable<T> {
     try {
-        return JSON.parse(text);
+        return JSON.parse(text as Any);
     } catch {
         return undefined;
     }
