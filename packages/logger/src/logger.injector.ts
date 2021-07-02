44a3c8ff9ec49995b06635b8809df9546a8a3078
splincode <omaxphp@yandex.ru>
2021-07-02 17:12:10 +0300

    feat(@angular-ru/eslint-config): required new line after import

--- packages/logger/src/logger.injector.ts
+++ packages/logger/src/logger.injector.ts
@@ -1,6 +1,7 @@
 import { Injectable, Injector } from '@angular/core';
 import { Nullable } from '@angular-ru/common/typings';
 import { isNil } from '@angular-ru/common/utils';
+
 @Injectable()
 export class LoggerInjector {
     private static injector: Nullable<Injector> = null;
