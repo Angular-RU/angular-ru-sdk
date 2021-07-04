10f3328d8b97771a195e38adde3a0b3eaf79d49d
Max Ivanov <omaxphp@yandex.ru>
2021-07-04 22:39:12 +0300

    build: use es2015 by default (#717)

--- packages/common/integration/tests/nodejs/ts-utils.spec.ts
+++ packages/common/integration/tests/nodejs/ts-utils.spec.ts
@@ -7,7 +7,7 @@ describe('[TEST]: TS Utils', () => {
         );
 
         expect(exposeTsCompilerOptionsByTsConfig('./tsconfig.lib.json').paths).toEqual({
-            '@angular-ru/common/*': ['*/src/public_api.ts']
+            '@angular-ru/common/*': ['dist/library/common/dist/library/*']
         });
     });
 });
