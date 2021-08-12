20ee060f12da121d359488d24d87f77b3f6f5c8e
splincode <omaxphp@yandex.ru>
2021-08-12 19:03:05 +0300

    feat: skip scope enum

--- packages/commitlint/index.js
+++ packages/commitlint/index.js
@@ -3,7 +3,7 @@ module.exports = {
     rules: {
         'header-max-length': [2, 'always', 100],
         'subject-case': [0],
-        'scope-enum': [2, 'never'],
+        'scope-enum': [0],
         'type-enum': [
             2,
             /**
