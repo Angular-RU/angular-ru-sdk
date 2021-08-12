50ad9452b7037239a685acc4487dcf640139f878
splincode <omaxphp@yandex.ru>
2021-08-12 18:57:46 +0300

    feat: support chore prefix

--- packages/commitlint/index.js
+++ packages/commitlint/index.js
@@ -6,6 +6,17 @@ module.exports = {
         'scope-enum': [2, 'never'],
         'type-enum': [
             2,
+            /**
+             * feat: (new feature for the user, not a new feature for build script)
+             * fix: (bug fix for the user, not a fix to a build script)
+             * docs: (changes to the documentation)
+             * ci: (update ci jobs)
+             * build: (changes for build pipeline)
+             * style: (formatting, missing semi colons, etc; no production code change)
+             * refactor: (refactoring production code, eg. renaming a variable)
+             * test: (adding missing tests, refactoring tests; no production code change)
+             * chore: (no production code change)
+             */
             'always',
             ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'chore']
         ]
