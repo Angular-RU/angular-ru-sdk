41be6ee6dd527f50c820c740877c0dc52eafddf3
splincode <omaxphp@yandex.ru>
2021-08-12 18:54:13 +0300

    feat: support chore prefix

--- packages/commitlint/index.js
+++ packages/commitlint/index.js
@@ -2,6 +2,12 @@ module.exports = {
     extends: ['@commitlint/config-conventional', '@commitlint/config-angular'],
     rules: {
         'header-max-length': [2, 'always', 100],
-        'subject-case': [0]
+        'subject-case': [0],
+        'scope-enum': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'chore']
+        ]
     }
 };
