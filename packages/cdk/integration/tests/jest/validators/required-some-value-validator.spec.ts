97d7a5e1abd8b6baa81b3ad1a9884e1cf5909cf0
Mansur Ishmuhametov <34547075+mansurishmuhametov@users.noreply.github.com>
2021-09-13 15:10:29 +0300

    feat: added requiredSomeValueValidator (#936)
    Co-authored-by: Мансур Ишмухаметов <m.ishmukhametov@tinkoff.ru>

---
+++ packages/cdk/integration/tests/jest/validators/required-some-value-validator.spec.ts
@@ -0,0 +1,67 @@
+import { AbstractControl, FormControl, FormGroup, ValidatorFn } from '@angular/forms';
+import { requiredSomeValueValidator } from '@angular-ru/cdk/validators';
+
+describe('[TEST]: requiredSomeValueValidator', () => {
+    let form: FormGroup;
+    beforeEach(() => {
+        form = new FormGroup(
+            {
+                aaa: new FormControl(),
+                bbb: new FormControl(),
+                ccc: new FormControl()
+            },
+            { validators: [requiredSomeValueValidator()] }
+        );
+    });
+
+    it('should return error if control is not part of FormGroup', () => {
+        const control: AbstractControl = new FormControl();
+        const validator: ValidatorFn = requiredSomeValueValidator();
+        expect(() => validator(control))?.toThrow(new Error('requiredSomeValue must be used on form group'));
+    });
+
+    it('should return error if all controls with no values: undefined, null, NaN', () => {
+        form.controls.aaa?.setValue(undefined);
+        form.controls.bbb?.setValue(null);
+        form.controls.ccc?.setValue(NaN);
+        expect(form.errors).toEqual({ requiredSomeValue: true });
+    });
+
+    it('should return error if all controls with no values: "", "null", Infinity', () => {
+        form.controls.aaa?.setValue('');
+        form.controls.bbb?.setValue(null);
+        form.controls.ccc?.setValue(Infinity);
+        expect(form.errors).toEqual({ requiredSomeValue: true });
+    });
+
+    it('should return error if all controls with no values: []', () => {
+        form.controls.aaa?.setValue([]);
+        expect(form.errors).toEqual({ requiredSomeValue: true });
+    });
+
+    it('should return error if all controls with no values', () => {
+        expect(form.errors).toEqual({ requiredSomeValue: true });
+    });
+
+    it('should be valid if there is only one value with type number', () => {
+        form.controls.aaa?.setValue(13);
+        expect(form.valid).toEqual(true);
+    });
+
+    it('should be valid if there is only one value with type string', () => {
+        form.controls.bbb?.setValue('awesome');
+        expect(form.valid).toEqual(true);
+    });
+
+    it('should be valid if there is only one value with type Object', () => {
+        form.controls.ccc?.setValue({});
+        expect(form.valid).toEqual(true);
+    });
+
+    it('should be valid if there is more than one value', () => {
+        form.controls.aaa?.setValue(13);
+        form.controls.bbb?.setValue('awesome');
+        form.controls.ccc?.setValue({});
+        expect(form.valid).toEqual(true);
+    });
+});
