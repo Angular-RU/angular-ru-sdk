baa7a8cf3ae6f657a4e911947911515b86921aff
Nikita Sadchikov <fiveoufive@gmail.com>
2021-09-27 13:21:27 +0500

    fixup! feat(cdk): fix path to typings

--- packages/cdk/utils/src/is-boolean.ts
+++ packages/cdk/utils/src/is-boolean.ts
@@ -1,4 +1,4 @@
-import { Any } from '../../dist/library/typings';
+import { Any } from '@angular-ru/cdk/typings';
 
 export function isBoolean(value: Any): value is boolean {
     return typeof value === 'boolean';
