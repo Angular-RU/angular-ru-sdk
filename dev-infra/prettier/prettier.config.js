2891c1df27418c58466d56f17d5f06c8bb9c4c30
splincode <omaxphp@yandex.ru>
2021-11-18 23:04:46 +0300

    feat(prettier): add xml plugin

--- dev-infra/prettier/prettier.config.js
+++ dev-infra/prettier/prettier.config.js
@@ -7,7 +7,7 @@ module.exports = {
     singleQuote: true,
     trailingComma: 'none',
     htmlWhitespaceSensitivity: 'ignore',
-    plugins: ['prettier-plugin-organize-attributes'],
+    plugins: ['prettier-plugin-organize-attributes', '@prettier/plugin-xml'],
     bracketSameLine: false,
     bracketSpacing: true,
     arrowParens: 'always',
@@ -24,5 +24,43 @@ module.exports = {
         '$ANGULAR_INPUT',
         '$ANGULAR_TWO_WAY_BINDING',
         '$ANGULAR_OUTPUT'
+    ],
+    overrides: [
+        {
+            files: ['*.js', '*.ts'],
+            options: { parser: 'typescript' }
+        },
+        {
+            files: ['*.md'],
+            options: { parser: 'markdown' }
+        },
+        {
+            files: ['*.json', '.prettierrc', '.stylelintrc'],
+            options: { parser: 'json' }
+        },
+        {
+            files: ['*.less'],
+            options: { parser: 'less' }
+        },
+        {
+            files: ['*.scss'],
+            options: { parser: 'scss' }
+        },
+        {
+            files: ['*.html'],
+            options: { parser: 'html' }
+        },
+        {
+            files: ['*.template.html', '*.component.html'],
+            options: { parser: 'angular' }
+        },
+        {
+            files: ['*.svg'],
+            options: { parser: 'xml' }
+        },
+        {
+            files: ['*.xml'],
+            options: { parser: 'xml' }
+        }
     ]
 };
