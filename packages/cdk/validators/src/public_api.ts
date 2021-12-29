4f93fb10e2b07489bb827e2dba094b63ad304420
Mansur Ishmuhametov <34547075+mansurishmuhametov@users.noreply.github.com>
2021-12-29 12:40:58 +0300

    feat: added util longValidator (#1173)
    * feat: added util longValidator
    
    * fix: lint errors
    
    Co-authored-by: Мансур Ишмухаметов <m.ishmukhametov@tinkoff.ru>

--- packages/cdk/validators/src/public_api.ts
+++ packages/cdk/validators/src/public_api.ts
@@ -4,8 +4,10 @@ export {
     dateMinIntervalValidator,
     orderedIntervalValidator
 } from './date-interval.validator';
+// eslint-disable-next-line deprecation/deprecation
 export { emptyValidator, EmptyValidatorFn } from './empty.validator';
 export { everyArrayItemValidator } from './every-array-item.validator';
 export { maxDateValidator, minDateValidator } from './limit-date.validator';
+export { longValidator } from './long.validator';
 export { requiredSomeValueValidator } from './required-some-value.validator';
 export { requiredSomeValueByKeysValidator } from './required-some-value-by-keys.validator';
