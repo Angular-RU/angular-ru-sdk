d50d487740be36f5e6e180150726769083a2639e
splincode <omaxphp@yandex.ru>
2022-01-08 18:04:36 +0300

    chore: research

--- .scripts/clean.ts
+++ .scripts/clean.ts
@@ -5,7 +5,7 @@ import { noThrow } from './utils/no-throw';
 void (async function main(): Promise<void> {
     info(`CLEAN OLD BUILDS/CACHE`);
 
-    const commands: string[] = [
+        const commands: string[] = [
         `rm -rf node_modules/.cache`,
         `rm -rf packages/**/dist packages/**/.cache packages/**/node_modules/.cache`,
         `rm -rf packages/**/dist packages/**/.cache packages/**/node_modules/.cache`,
