732b0ce32c945a82568772eacf3ac2ff045f2472
Schirbak <33288503+schirbak@users.noreply.github.com>
2022-01-21 17:26:49 +0300

    feat(cdk): correct `trim` function signature

--- libs/cdk/string/src/trim.ts
+++ libs/cdk/string/src/trim.ts
@@ -1,6 +1,4 @@
-import { Nullable } from '@angular-ru/cdk/typings';
-
-export function trim(value?: Nullable<string>): Nullable<string> {
+export function trim<T extends string | null | undefined>(value: T): (T & string) | (T & null) | (T & undefined) {
     // note: don't use isString for preserve circular dependencies
-    return typeof value === 'string' ? value?.trim() : value;
+    return (typeof value === 'string' ? value?.trim() : value) as T;
 }
