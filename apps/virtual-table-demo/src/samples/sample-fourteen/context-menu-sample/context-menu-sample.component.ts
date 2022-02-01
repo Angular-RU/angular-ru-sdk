033104aab6e4bb171db47415e8928d6545ec2b67
Schirbak <33288503+schirbak@users.noreply.github.com>
2022-02-01 13:10:50 +0300

    feat(cdk): table filter type IS_FILLED

--- apps/virtual-table-demo/src/samples/sample-fourteen/context-menu-sample/context-menu-sample.component.ts
+++ apps/virtual-table-demo/src/samples/sample-fourteen/context-menu-sample/context-menu-sample.component.ts
@@ -1,10 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FilterStateEvent, TableBuilderComponent, TableFilterType } from '@angular-ru/cdk/virtual-table';
-import { WebWorkerThreadService } from '@angular-ru/cdk/webworker';
-
-WebWorkerThreadService.prototype.run = function run<T, K>(workerFunction: (input: K) => T, data?: K): Promise<T> {
-    return Promise.resolve(workerFunction(data!));
-};
 
 @Component({
     selector: 'context-menu-sample',
