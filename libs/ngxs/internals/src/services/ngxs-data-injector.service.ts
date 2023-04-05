f7d06256197ad664098387f7739419e71fca6ae7
gromon42 <gaultieromon@gmail.com>
2023-04-05 16:47:30 +0200

    fix(ngxs): updated import from ngxs/store

--- libs/ngxs/internals/src/services/ngxs-data-injector.service.ts
+++ libs/ngxs/internals/src/services/ngxs-data-injector.service.ts
@@ -1,6 +1,6 @@
 import { Inject, Injectable, Injector, NgZone } from '@angular/core';
 import { Store } from '@ngxs/store';
-import { NGXS_STATE_CONTEXT_FACTORY, NGXS_STATE_FACTORY } from '@ngxs/store/internals';
+import { ɵNGXS_STATE_CONTEXT_FACTORY, ɵNGXS_STATE_FACTORY } from '@ngxs/store/internals';
 
 import { NgxsDataSequence } from './ngxs-data-computed-stream.service';
 
@@ -15,8 +15,8 @@ export class NgxsDataInjector {
 
     constructor(
         injector: Injector,
-        @Inject(NGXS_STATE_FACTORY) stateFactory: unknown | any,
-        @Inject(NGXS_STATE_CONTEXT_FACTORY) stateContextFactory: unknown | any
+        @Inject(ɵNGXS_STATE_FACTORY) stateFactory: unknown | any,
+        @Inject(ɵNGXS_STATE_CONTEXT_FACTORY) stateContextFactory: unknown | any
     ) {
         NgxsDataInjector.store = injector.get<Store>(Store);
         NgxsDataInjector.ngZone = injector.get<NgZone>(NgZone);
