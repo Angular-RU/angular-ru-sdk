36a5d0c1a906e1d2eb430cccb8cc509de15e2ddf
Markiewic <33288503+Markiewic@users.noreply.github.com>
2023-04-24 22:15:45 +0300

    refactor: remove deprecated `accessibility-label-for` rule
    The rule `@angular-eslint/template/accessibility-label-for` is deprecated: https://github.com/angular-eslint/angular-eslint/blob/main/packages/eslint-plugin-template/docs/rules/accessibility-label-for.md#%EF%B8%8F-this-rule-is-deprecated

--- libs/eslint-config-enterprise/modules/angular-eslint.js
+++ libs/eslint-config-enterprise/modules/angular-eslint.js
@@ -49,7 +49,6 @@ module.exports = {
                 'max-len': 'off',
                 '@angular-eslint/template/accessibility-alt-text': 'error',
                 '@angular-eslint/template/accessibility-elements-content': 'error',
-                '@angular-eslint/template/accessibility-label-for': 'error',
                 '@angular-eslint/template/accessibility-label-has-associated-control': 'error',
                 '@angular-eslint/template/accessibility-table-scope': 'error',
                 '@angular-eslint/template/accessibility-valid-aria': 'error',
