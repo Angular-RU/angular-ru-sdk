8b69118d1adebbb8f81a67b02a92ad6cb13813d0
Markiewic <33288503+schirbak@users.noreply.github.com>
2023-10-21 12:26:56 +0300

    fix: some editings

--- libs/cdk/src/_noop.ts
+++ libs/cdk/src/_noop.ts
@@ -1,2 +1,3 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export function _noop(): void {}
+//
--- libs/ngxs/src/public_api.ts
+++ libs/ngxs/src/public_api.ts
@@ -1,2 +1,3 @@
 export { NGXS_DATA_CONFIG, NgxsDataConfig } from './config/ngx-data.config';
 export { NgxsDataPluginModule } from './ngxs-data.module';
+//
