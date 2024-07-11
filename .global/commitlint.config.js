c6a58e96c5ef52442f5f96a9d9c7172abdb9c52b
splincode <omaxphp@yandex.ru>
2024-07-11 23:14:24 +0300

    ci: update

--- .global/commitlint.config.js
+++
@@ -1,6 +0,0 @@
-const {projects} = require('../workspace.json');
-
-module.exports = {
-    extends: [require('path').resolve(__dirname, '../libs/commitlint/index.js')],
-    rules: {'scope-enum': () => [2, 'always', [...Object.keys(projects), 'release']]},
-};
