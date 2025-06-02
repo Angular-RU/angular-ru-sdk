b447b96f4656bda677f2ae776bf58cfe036823d2
Tomas Rimkus <sandshrew@gmail.com>
2025-06-02 17:25:15 +0200

    fix: introduce Angular v20 compatibility by removing "InjectFlags" usage (#1532)

--- libs/cdk/ivy/utils/injection-utils.ts
+++ libs/cdk/ivy/utils/injection-utils.ts
@@ -1,7 +1,6 @@
 /* eslint-disable no-redeclare */
-import {AbstractType, InjectionToken, Type} from '@angular/core';
+import {AbstractType, InjectionToken, InjectOptions, Type} from '@angular/core';
 import {
-    InjectFlags,
     ɵɵdirectiveInject as ivyDirectiveInject,
     ɵɵinject as ivyInject,
 } from '@angular/core';
@@ -10,16 +9,16 @@ import {Fn, Nullable} from '@angular-ru/cdk/typings';
 function wrapperForInject<T>(
     wrap: Fn,
     token: AbstractType<T> | InjectionToken<T> | Type<T>,
-    flags?: InjectFlags,
+    options?: InjectOptions,
 ): Nullable<T> {
-    if (InjectFlags.Optional) {
+    if (options?.optional) {
         try {
-            return wrap(token, flags!);
+            return wrap(token, options);
         } catch {
             return null;
         }
     } else {
-        return flags ? wrap(token, flags) : wrap(token);
+        return options ? wrap(token, options) : wrap(token);
     }
 }
 
@@ -31,14 +30,14 @@ export function directiveInject<T>(
 ): T;
 export function directiveInject<T>(
     token: AbstractType<T> | InjectionToken<T> | Type<T>,
-    flags: InjectFlags,
+    options: InjectOptions,
 ): Nullable<T>;
 
 export function directiveInject<T>(
     token: AbstractType<T> | InjectionToken<T> | Type<T>,
-    flags?: InjectFlags,
+    options?: InjectOptions,
 ): Nullable<T> {
-    return wrapperForInject(ivyDirectiveInject, token, flags);
+    return wrapperForInject(ivyDirectiveInject, token, options);
 }
 
 /**
@@ -47,12 +46,12 @@ export function directiveInject<T>(
 export function inject<T>(token: AbstractType<T> | InjectionToken<T> | Type<T>): T;
 export function inject<T>(
     token: AbstractType<T> | InjectionToken<T> | Type<T>,
-    flags: InjectFlags,
+    options: InjectOptions,
 ): Nullable<T>;
 
 export function inject<T>(
     token: AbstractType<T> | InjectionToken<T> | Type<T>,
-    flags?: InjectFlags,
+    options?: InjectOptions,
 ): Nullable<T> {
-    return wrapperForInject(ivyInject, token, flags);
+    return wrapperForInject(ivyInject, token, options);
 }
