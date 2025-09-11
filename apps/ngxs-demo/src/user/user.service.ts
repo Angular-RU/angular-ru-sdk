c6c054aa074480c63f487077c3bb991706761851
Tomas Rimkus <sandshrew@gmail.com>
2025-09-11 12:30:31 +0300

    chore(ngxs-demo): fixed "user.json" loading and clearing of todo input

--- apps/ngxs-demo/src/user/user.service.ts
+++ apps/ngxs-demo/src/user/user.service.ts
@@ -14,7 +14,9 @@ export class UserService {
     private readonly SIMULATE_REQUEST_DELAY: number = 2000;
 
     public loadUser(): Observable<UserModel> {
-        return this.httpService.get<{data: UserModel}>('/assets/user.json').pipe(
+        const path = location.pathname;
+
+        return this.httpService.get<{data: UserModel}>(`${path}assets/user.json`).pipe(
             delay(this.SIMULATE_REQUEST_DELAY),
             map((response: {data: UserModel}): UserModel => response.data),
         );
